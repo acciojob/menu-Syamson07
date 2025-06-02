52c1ec62ba33da2b8bcdf34c5a7bc4f2ef8f37ec
Syam Son Penumala <syamson132000@gmail.com>
2025-06-02 21:02:48 +0530

    Update Menu.js

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -3,19 +3,19 @@ import React from "react";
 const Menu = ({ items }) => {
   return (
     <div className="menu-container">
-      {items.map((item) => {
-        const dataTestId = `menu-item-${item.category.toLowerCase()}`;
-        return (
-          <div className="menu-item" key={item.id} data-test-id={dataTestId}>
-            <img src={item.image} alt={item.name} className="menu-img" />
-            <div className="menu-info">
-              <h4>{item.name}</h4>
-              <p>{item.price}</p>
-              <small>{item.category}</small>
-            </div>
+      {items.map((item) => (
+        <div
+          className="menu-item"
+          key={item.id}
+          data-test-id={`menu-item-${item.category.toLowerCase()}`}
+        >
+          <img src={item.image} alt={item.name} className="menu-img" />
+          <div className="menu-info">
+            <h4>{item.name}</h4>
+            <p>{item.price}</p>
           </div>
-        );
-      })}
+        </div>
+      ))}
     </div>
   );
 };
